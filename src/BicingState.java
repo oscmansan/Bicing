52f3f0e236e7534bd08103311eccf79c5a9e77df
JFonS <joan.fonssanchez@gmail.com>
2015-10-04 18:44:23 +0200

    Definits operadors i getters

--- src/BicingState.java
+++ src/BicingState.java
@@ -1,9 +1,10 @@
 import IA.Bicing.Estaciones;
 
 public class BicingState {
-
     public static Estaciones estaciones;
     public static int nFurgos;
+    public static final int DESTI1 = 1;
+    public static final int DESTI2 = 3;
     public int[][] furgos;
     /*
       [0] Estacio d'origen
@@ -11,8 +12,8 @@ public class BicingState {
       [2] Numero de bicis a (1)
       [3] Estacio de desti  (2)
       [4] Numero de bicis a (2)
+    */
 
-      */
     public BicingState(int nest, int nbic, int dem, int seed, int nf) {
         nFurgos = nf;
         estaciones = new Estaciones(nest, nbic, dem, seed);
@@ -32,5 +33,31 @@ public class BicingState {
             b.furgos[i][4] = furgos[i][4];
         }
         return b;
+    }
+
+    //Operadors
+    public final void canviarOrigen(int i, int estacio) {
+        furgos[i][0] = estacio;
+    }
+
+    public final void canviarDesti(int i, int desti, int estacio) {
+        furgos[i][desti] = estacio;
+    }
+
+    public final void canviarNumBicis(int i, int desti, int nBicis) {
+        furgos[i][desti+1] = nBicis;
+    }
+
+    //Getters
+    public final int getOrigen(int i) {
+        return furgos[i][0];
+    }
+
+    public final int getDesti(int i, int desti) {
+        return furgos[i][desti];
+    }
+
+    public final int getNumBicis(int i, int desti) {
+        return furgos[i][desti+1];
     }
 }
